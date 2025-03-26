a898ffb08e8491d9e99db5f651b56ade2d41071e
JulianoBC <jbcap@hotmail.com>
2025-03-26 19:17:20 -0300

    inclusão de tests automatizados por ia

--- src/config/site.ts
+++ src/config/site.ts
@@ -6,14 +6,14 @@ interface SocialLinks {
 interface SiteConfig {
   title: string;
   description: string;
-  profileImage: string;
+  profileImage: "/perfilfacebook.jpg",
   socialLinks: SocialLinks;
 }
 
 export const siteConfig: SiteConfig = {
   title: "Juliano BC",
   description: "Desenvolvedor FullStack que transforma curiosidade em código. Meu maior prazer é aprender coisas novas e aplicar esse conhecimento em soluções reais.",
-  profileImage: "https://media.licdn.com/dms/image/v2/D4D03AQG_9cJVEwqgzQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1666726671679?e=1738195200&v=beta&t=2VXRQu1nrejHTIUAEOT-oouCvKUV8rBtTU7krzGnbCs",
+  profileImage: "/perfilfacebook.jpg",
   socialLinks: {
     github: "https://github.com/JulianoBC",
     linkedin: "https://www.linkedin.com/in/juliano-beig-capretz/",
